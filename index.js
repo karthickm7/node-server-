2d25712aff182d1398d51f3205f822a762c48185
karthickm7 <karthickmohan@dorustree.in>
2022-05-05 20:02:10 +0530

    create signtokens

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { user } = require("./user");
 const app = express();
+const jwt = require("jsonwebtoken");
 app.use(express.json());
 const port = 3004;
 
@@ -9,7 +10,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signup", (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, email } = req.body;
   console.log("signup", user);
   let check = user.find((el) => el.username === username);
   if (check) {
@@ -19,13 +20,38 @@ app.post("/signup", (req, res) => {
     });
   }
 
-  user.push({ username, password });
+  user.push({ username, password, email });
+
+  const token = jwt.sign({ email }, "asdfghjkl");
+
   console.log(user);
   res.status(200).json({
     status: "success",
+    token,
   });
 });
 
+app.post("/login", (req, res) => {
+  const { password, email } = req.body;
+  console.log("login", user);
+  let check = user.find((el) => el.email === email);
+  if (check) {
+    res.status(401).json({
+      status: "error",
+      message: "email already exist",
+    });
+  }
+
+  user.push({ password, email });
+
+  const token = jwt.sign({ email }, "asdfghjkl");
+  const refreshtoken = jwt.sign({ email }, "");
+  console.log(user);
+  res.status(200).json({
+    status: "success",
+    token,
+  });
+});
 app.listen(port, () => {
   console.log("hai", `${port}`);
 });
